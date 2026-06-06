c6207de7eb43b8a9233d71436868ddaf1837c74f
Md Tasin Tahmid <md.tasin.tahmid1998@gmail.com>
2026-06-07 03:36:49 +0600

    Add books and authors table on pg

---
+++ models/book.model.js
@@ -0,0 +1,13 @@
+const { uuid, text, integer, pgTable, varchar } = require("drizzle-orm/pg-core");
+const authorsTable = require("./author.model");
+
+const booksTable = pgTable("books", {
+  id: uuid().primaryKey().defaultRandom(),
+  title: varchar({ length: 100 }).notNull(),
+  description: text(),
+  authorId: uuid()
+    .references(() => authorsTable.id)
+    .notNull(),
+});
+
+module.exports = booksTable;
