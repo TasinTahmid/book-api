71f9f4f1c865a55daf4130f9140e398ad70d095c
Md Tasin Tahmid <md.tasin.tahmid1998@gmail.com>
2026-06-08 11:16:56 +0600

    Add all the book controllers

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,25 +1,77 @@
-exports.getAllBooks = (req, res) => {
-    console.log("book api is called::")
-    res.send("Got all books!!");
+const booksTable = require("../models/book.model");
+const db = require("../db");
+const { eq } = require("drizzle-orm");
+
+exports.getAllBooks = async (req, res) => {
+	const books = await db.select().from(booksTable);
+	res.json(books);
 };
 
-exports.createBook = (req, res) => {
-    console.log("book api is called::")
-    res.send("Book created successfully!!");
+exports.getBookById = async (req, res) => {
+	const id = req.params.id;
+
+	const [book] = await db
+		.select()
+		.from(booksTable)
+		.where((table) => eq(table.id, id))
+		.limit(1);
+
+	if (!book) return res.status(404).json({ error: `Book not found!` });
+
+	res.json(book);
 };
 
-exports.getBookById = (req, res) => {
-    console.log("book api is called::", req.params.id)
-    res.send("Get book id by "+ req.params.id);
+exports.createBook = async (req, res) => {
+	const { title, description, authorId } = req.body;
+
+	if (!title || title === "")
+		res.status(404).json({ error: "Title is required" });
+
+	const [result] = await db
+		.insert(booksTable)
+		.values({
+			title,
+			authorId,
+			description,
+		})
+		.returning({
+			id: booksTable.id,
+		});
+
+	res.status(201).json({
+		message: "Book created successfully",
+		id: result.id,
+	});
 };
 
-exports.updateBookById = (req, res) => {
-    console.log("book api is called::", req.params.id)
-    res.send("Update book id by "+ req.params.id);
+exports.updateBookById = async (req, res) => {
+	const id = req.params.id;
+	const { title, description, authorId } = req.body;
+
+	const [updatedBook] = await db
+		.update(booksTable)
+		.set({
+			title,
+			description,
+			authorId,
+		})
+		.where(eq(booksTable.id, id))
+		.returning();
+
+	if (!updatedBook) {
+		return res.status(404).json({ error: "Book not found!" });
+	}
+
+	res.json({
+		message: "Book updated successfully!",
+		book: updatedBook,
+	});
 };
 
-exports.deleteBookById = (req, res) => {
-    console.log("book api is called::", req.params.id)
-    res.send("Deleted book id by "+ req.params.id);
-};
+exports.deleteBookById = async (req, res) => {
+	const id = req.params.id;
 
+	await db.delete(booksTable).where(eq(booksTable.id, id));
+
+	res.json({ message: "Book deleted successfully!" });
+};
